0f44ff91dfbdf0fadb6fc9afd883a0ed8f246ab9
Drew Rogers <drewbikscube@icloud.com>
2023-01-26 12:36:27 -0800

    todo app

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+
+        return view('index', compact('todos'));
+    }
+
+    public function create(Request $request)
+    {
+        $data = $request->validate([
+            'task' => 'required'
+        ]);
+
+        Todo::create($data);
+
+        return back();
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'status' => Rule::in(['active', 'completed'])
+        ]);
+
+        $todo->update([
+            'status' => $request->input('status')
+        ]);
+
+        return back();
+    }
+
+    public function delete(Todo $todo)
+    {
+        $todo->delete();
+        
+        return back();
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todos</title>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body class="bg-secondary">
+    <div class="container w-100 w-lg-50 mt-5">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h3>Tasks</h3>
+                <form action="{{ route('create') }}" method="POST">
+                    @csrf
+                    <div class="input-group">
+                        <input type="text" name="task" class="form-control" placeholder="Add a new task">
+                        <button type="submit" class="btn btn-dark btn-sm px-4 shadow-sm"><i class="fas fa-plus"></i></button>
+                    </div>
+                </form>
+                @if(count($todos))
+                <ul class="list-group list-group-flush mt-3">
+                    @foreach ($todos as $todo)
+                    <li class="list-group-item d-flex">
+                        <form class="w-100 d-flex flex-grow-2" action="{{ route('update', $todo->id) }}" method="POST">
+                            <div class="w-100">
+                                @if($todo->status == 'completed')
+                                <del>
+                                @endif
+                                    <div>{{ $todo->task }}</div>
+                                @if($todo->status == 'completed')
+                                </del>
+                                @endif
+                            </div>
+                            @csrf
+                            @method('patch')
+                            @if($todo->status == 'active')
+                            <input type="hidden" name="status" value="completed">
+                            <button type="submit" class="btn btn-link btn-sm float-end"><i class="fas fa-check"></i></button>
+                            @endif
+                        </form>
+                        <form action="{{ route('delete', $todo->id) }}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-link btn-sm float-end" onclick='return confirm("Are you sure?");'><i class="fas fa-trash"></i></button>
+                        </form>
+                    </li>
+                    @endforeach
+                </ul>
+                @endif
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
